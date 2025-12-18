22e685036902c3c781b57cf8e45317ce1f46c0cf
GonzaloPDM <gonzalopantindm@gmail.com>
2025-12-18 21:02:02 +0100

    road_lines.py changed to road_lines_soble.py

---
+++ road_lines_sobel.py
@@ -0,0 +1,359 @@
+import os
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def road_line_filter_image(image, saturation_thresh=(170, 255), sobelx_thresh=(20, 100)):
+    """ Filter the road lines in the image using color and gradient thresholds.
+    
+        Args:
+            image: Input image to be filtered
+            saturation_thresh: Saturation threshold
+            sobelx_thresh: Sobel x threshold
+        Returns: 
+            combined_binary: Binary image after applying the filters
+    """
+    # Transform to HLS color space
+    hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS).astype(float)
+    h_channel = hls[:,:,0] # Hue channel
+    l_channel = hls[:,:,1] # Lightness channel
+    s_channel = hls[:,:,2] # Saturation channel
+
+    # Filter white pixels (Hihg luminosity)
+    white_mask = np.zeros_like(l_channel)
+    white_mask[(l_channel > 220) & (l_channel <= 255)] = 1
+
+    # Filter yellow pixels (High saturation + Hue: 15 - 35)
+    yellow_mask = np.zeros_like(s_channel)
+    yellow_mask[((h_channel >= 15) & (h_channel <= 35)) & 
+                ((s_channel > saturation_thresh[0]) & (s_channel <= saturation_thresh[1]))] = 1
+    
+    # Sobel X: vertical edges
+    l_blur = cv2.GaussianBlur(l_channel, (5,5), 1)
+    sobel_x = cv2.Sobel(l_blur, cv2.CV_64F, 1, 0)
+    abs_sobel_x = np.absolute(sobel_x)
+    scaled_sobel = np.uint8(255*abs_sobel_x/np.max(abs_sobel_x))
+    
+    sobelx_mask = np.zeros_like(scaled_sobel)
+    sobelx_mask[(scaled_sobel >= sobelx_thresh[0]) & (scaled_sobel <= sobelx_thresh[1])] = 1
+    
+    # Create final mask
+    color_binary = np.dstack((np.zeros_like(sobelx_mask), sobelx_mask, (yellow_mask==1) | (white_mask==1))) * 255
+    
+    combined_binary = np.zeros_like(sobelx_mask)
+    combined_binary[(yellow_mask == 1) | (white_mask == 1) | (sobelx_mask == 1)] = 1
+
+    # Eliminate car hood from image
+    #h, _ = combined_binary.shape
+    #hood_height = int(h * 0.05) # Around 5% of bottom pixels belongs to the hood
+    #combined_binary[h-hood_height:h, :] = 0
+    
+    return combined_binary, color_binary
+
+
+
+def remove_road_perspective(image, 
+                            img_h, img_w,
+                            src_pts, dst_pts):
+    """
+        This method calculate the homography of the road, given the points of a rectangle aling with straight road lines
+    Args:
+        img: The road image from the camera perspective
+        src_pts: The points of a rectangle align with the road lines in the image. In case its not given, it will be aproximated
+                    by a default rectangle taken from a dashboard camera.
+
+    Returns:
+        no_perspective_image: The image with removed perspective, like seen from above
+        M: The homography matrix calculated for removing perspective
+    """
+    M = cv2.getPerspectiveTransform(src_pts, dst_pts)
+    
+    no_perspective_image = cv2.warpPerspective(image, M, (img_w, img_h), flags=cv2.INTER_LINEAR)
+
+    return no_perspective_image, M
+
+
+
+def calculate_road_lines(binary_warped, draw_points=False, draw_windows=False, ):
+    """ Calculate the road lines in a edges image with non perspective transformation applied, using sliding windows and polynomial fitting. 
+        Then, draw the windows and the lines in an output image.
+    
+        Args:
+            binary_warped: The binary image with the road lines in white
+        Returns:
+            left_fit: The polynomial coefficients for the left road line
+            right_fit: The polynomial coefficients for the right road line
+            out_img: The output image with the detected lines and windows drawn
+    """  
+    img_h, img_w = binary_warped.shape[:2]
+
+    # Take a histogram of the bottom half of the image
+    histogram = np.sum(binary_warped[int(img_h*0.8):int(img_h*0.98),int(img_w*0.35):int(img_w*0.65)], axis=0)
+
+    # The peak of the left and right halves of the histogram will be the starting point for the left and right lines
+    mid_point = histogram.shape[0]//2
+    left_peak = np.argmax(histogram[:mid_point]) + int(img_w*0.35)
+    right_peak = np.argmax(histogram[mid_point:]) + mid_point + int(img_w*0.35)
+
+    if left_peak == 0 or right_peak == 0:
+        return None, None, None
+
+    # Set parameters for sliding windows
+    nwindows = 9
+    window_height = int(img_h // nwindows)
+    margin = img_w // 20
+    minpix = 50
+    left_current = left_peak 
+    right_current = right_peak
+    
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+
+    out_img = None
+
+    if draw_windows:
+        # Create an output image to draw on and visualize the result
+        out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+
+    # Slide windows from bottom to top, finding line pixels
+    for window in range(nwindows):
+        # Define new window
+        win_y_low = img_h - window * window_height
+        win_y_high = img_h - (window + 1) * window_height
+        win_xleft_low = left_current - margin
+        win_xleft_high = left_current + margin
+        win_xright_low = right_current - margin
+        win_xright_high = right_current + margin
+
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = (binary_warped[win_y_high:win_y_low, win_xleft_low:win_xleft_high]).nonzero()
+        good_right_inds = (binary_warped[win_y_high:win_y_low, win_xright_low:win_xright_high]).nonzero()
+
+        # Adjust pixels to image coordinates
+        good_left_inds = (good_left_inds[0] + win_y_high, good_left_inds[1] + win_xleft_low)
+        good_right_inds = (good_right_inds[0] + win_y_high, good_right_inds[1] + win_xright_low)
+
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+
+        new_left_current = left_current
+        new_right_current = right_current
+
+        # If found > minpix pixels, recenter next window on their mean position (ponderated to center of the image)
+        if len(good_left_inds[0]) > minpix:
+            new_left_current = int(np.mean(good_left_inds[1]))
+        if len(good_right_inds[0]) > minpix:
+            new_right_current = int(np.mean(good_right_inds[1]))
+
+        # Check wether boxes colides
+        left_box_right_edge = new_left_current + margin
+        right_box_left_edge = new_right_current - margin
+        buffer = 10 
+        
+        # If no colision, we let the means
+        if right_box_left_edge > (left_box_right_edge + buffer):
+            
+            left_current = new_left_current
+            right_current = new_right_current
+
+        # Else, we dont renew them
+        else:
+            pass
+
+        if draw_windows:
+            # Draw the windows on the visualization image
+            cv2.rectangle(out_img,
+                          (left_current - margin, win_y_high), 
+                          (left_current + margin, win_y_low),
+                          (0,255,0), 2)
+            cv2.rectangle(out_img,
+                          (right_current - margin, win_y_high), 
+                          (right_current + margin, win_y_low),
+                          (0,255,0), 2)
+
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds, axis=1)
+    right_lane_inds = np.concatenate(right_lane_inds, axis=1)
+
+    if (len(left_lane_inds[0]) == 0) | (len(right_lane_inds[0]) == 0):
+        return None, None, None
+
+    if draw_points:
+        # Draw lane pixels
+        out_img[left_lane_inds[0], left_lane_inds[1]] = [255, 0, 0]
+        out_img[right_lane_inds[0], right_lane_inds[1]] = [0, 0, 255]
+
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(left_lane_inds[0], left_lane_inds[1], 1)
+    right_fit = np.polyfit(right_lane_inds[0], right_lane_inds[1], 1)
+
+    return left_fit, right_fit, out_img
+
+
+def get_road_mask(no_perspective_image, left_fit, right_fit, Minv, h_img, img_w):
+    """
+        Create the mask of the detected road area
+    """
+    
+    # Calculate left and right lanes
+    ploty = np.linspace(0, h_img-1, h_img)
+    left_fitx = np.polyval(left_fit, ploty)
+    right_fitx = np.polyval(right_fit, ploty)
+    
+    # Create empty RGB image to draw the mask
+    warp_zero = np.zeros_like(no_perspective_image).astype(np.uint8)
+    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+    # Set format to points for cv2.fillpoly()
+    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    pts = np.hstack((pts_left, pts_right))
+
+    # Draw the mask on the empty image, this will be the mask
+    cv2.fillPoly(color_warp, np.int_([pts]), (0, 255, 0)) # Verde
+
+    # Apply inverse homography to the road mask
+    road_mask = cv2.warpPerspective(color_warp, Minv, (img_w, h_img)) 
+
+    return road_mask
+
+
+def process_lines_video(video_path, output_path,  
+                        src_pts=None, 
+                        dst_pts=None, 
+                        check_path=None
+                        ):
+    """
+        This functions processes a video by segmenting the own line of the vehicle.
+
+    Args:
+        video_path: The path to the video file
+        output_path: The path where the resulting video is written
+
+    Returns:
+        true if video is correctly processed, False if an error ocurred.
+    """
+    cap = cv2.VideoCapture(video_path)
+
+    if not cap.isOpened():
+        return False
+    else:
+        # Create Writer with same properties that the original video
+        img_w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        img_h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
+        out = cv2.VideoWriter(output_path, fourcc, fps, (img_w, img_h))
+
+        if src_pts is None:
+            p1_top_left = (int(img_w * 0.45), int(img_h * 0.63)) 
+            p2_top_right = (int(img_w * 0.52), int(img_h * 0.63)) 
+            p3_bot_right = (int(img_w * 0.84), int(img_h * 0.99)) 
+            p4_bot_left  = (int(img_w * 0.28), int(img_h * 0.99)) 
+
+            src_pts = np.float32([p1_top_left, p2_top_right, p3_bot_right, p4_bot_left])
+
+        if dst_pts is None:
+            dst_pts = np.float32([(int(img_w*0.4), 0), 
+                      (int(img_w*0.6), 0), 
+                      (int(img_w*0.6), img_h), 
+                      (int(img_w*0.4), img_h)
+                    ])
+
+        if check_path is not None:
+            out_check = cv2.VideoWriter(check_path, fourcc, fps, (img_w, img_h))
+        
+        print(f"Processing video... Resolution: {img_w}x{img_h}, FPS: {fps}")
+        
+        # Calculate homography
+        M = cv2.getPerspectiveTransform(src_pts, dst_pts)
+        Minv = np.linalg.inv(M)
+        
+        frame_count = 0
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break # Video ended
+
+            frame_processed = frame
+
+            # Filter potential line pixels
+            filter_frame, _ = road_line_filter_image(frame, sobelx_thresh=(20,100))
+
+            # Remove perspective
+            no_perspective_frame = cv2.warpPerspective(filter_frame, M, (img_w, img_h), flags=cv2.INTER_LINEAR)
+
+            # Calculate road lines
+            left_fit, right_fit, no_perspective_lines = calculate_road_lines(no_perspective_frame, draw_points=True, draw_windows=True)
+
+            if left_fit is not None and right_fit is not None:
+                road_mask = get_road_mask(no_perspective_frame, left_fit, right_fit, Minv, img_h, img_w)
+                
+                # Draw the mask on the original image
+                frame_processed = cv2.addWeighted(frame, 1, road_mask, 0.3, 0)
+
+            out.write(frame_processed)
+
+            if check_path is not None:
+                out_check.write(no_perspective_lines)
+            
+            # Show the frame processing in real time
+            cv2.imshow("Processed video", frame_processed)
+            
+            frame_count += 1
+            if frame_count % 50 == 0:
+                print(f"{frame_count} frames processed...")
+
+            # Exit if press 'q'
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        if frame_count == 0:
+            print("Error, video has not been processed")
+
+        # Liberar recursos
+        cap.release()
+        out.release()
+        cv2.destroyAllWindows()
+        return True
+    
+
+if __name__ == '__main__':
+
+    CWD = os.getcwd()
+    PATH_CITYSCAPE_DATA = os.path.join(CWD, 'datasets/cityscape_data')
+    PATH_IMAGES = os.path.join(CWD, "display_elements/road_segmentation/images")
+    PATH_VIDEOS = os.path.join(CWD, "display_elements/road_segmentation/videos")
+    PATH_RESULT_IMAGES = os.path.join(CWD, "results/road_segmentation/images")
+    PATH_RESULT_VIDEOS = os.path.join(CWD, "results/road_segmentation/videos")
+
+    try:
+        os.mkdir(PATH_RESULT_IMAGES)
+        os.mkdir(PATH_RESULT_VIDEOS)
+    except FileExistsError:
+        pass
+
+    print(f"Dataset: {PATH_CITYSCAPE_DATA}")
+    print(f"Images: {PATH_IMAGES}")
+    print(f"Videos: {PATH_VIDEOS}")
+    print(f"Resulting images: {PATH_RESULT_IMAGES}")
+    print(f"Resulting videos: {PATH_RESULT_VIDEOS}")
+    
+    # Get the video capture
+    video_name = "video1_recortado"
+    video_path = os.path.join(PATH_VIDEOS, f"{video_name}.mp4")
+    output_path = os.path.join(PATH_RESULT_VIDEOS,f"{video_name}_result_filter.mp4")
+    check_path = os.path.join(PATH_RESULT_VIDEOS, f"{video_name}_wraped_filter.mp4")
+
+    result = process_lines_video(video_path, output_path, check_path)
+
+    if result:
+        print(f"Video saved in: {output_path}")
+    else:
+        print("An error ocurred while openning the video.")
+
+
